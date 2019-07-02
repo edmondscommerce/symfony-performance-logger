e80f919a8460e42f2b841bd6504727f720fe8df9
ben <ben@ben>
2019-07-02 14:15:11 +0000

    Pass more context from the listener

--- src/PerformanceListener.php
+++ src/PerformanceListener.php
@@ -2,6 +2,12 @@
 
 namespace EdmondsCommerce\SymfonyPerformanceLogger;
 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+
 class PerformanceListener
 {
     private $performanceLogger;
@@ -15,27 +21,57 @@ class PerformanceListener
      * Console commands
      */
 
-    public function onConsoleCommand(): void
+    public function onConsoleCommand(ConsoleCommandEvent $event): void
     {
-        $this->performanceLogger->startLogging();
+        if ($this->isListCommand($event->getCommand())) {
+            return;
+        }
+
+        $this->performanceLogger->startLogging(
+            [
+                'command' => $event->getCommand()->getName(),
+            ]
+        );
     }
 
-    public function onConsoleTerminate(): void
+    public function onConsoleTerminate(ConsoleTerminateEvent $event): void
     {
-        $this->performanceLogger->endLogging();
+        if ($this->isListCommand($event->getCommand())) {
+            return;
+        }
+
+        $this->performanceLogger->endLogging(
+            [
+                'command' => $event->getCommand()->getName()
+            ]
+        );
     }
 
     /*
      * HTTP requests
      */
 
-    public function onKernelRequest(): void
+    public function onKernelRequest(GetResponseEvent $event): void
     {
-        $this->performanceLogger->startLogging();
+        $this->performanceLogger->startLogging(
+            [
+                'Request URI'    => $event->getRequest()->getRequestUri(),
+                'Request Params' => $event->getRequest()->request->all()
+            ]
+        );
     }
 
-    public function onKernelTerminate(): void
+    public function onKernelTerminate(PostResponseEvent $event): void
     {
-        $this->performanceLogger->endLogging();
+        $this->performanceLogger->endLogging(
+            [
+                'Request URI'    => $event->getRequest()->getRequestUri()
+            ]
+        );
+    }
+
+    private function isListCommand(Command $command): bool
+    {
+        return 'list' === $command->getName();
     }
 }
